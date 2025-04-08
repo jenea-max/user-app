42b7123196750eaeb665d2268e425be08aecfde9
Evghenia Gradeskova <egradeskova@edu.hse.ru>
2025-04-08 22:53:59 +0300

    Fix tsconfig for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000, 
@@ -16,5 +16,14 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 1000, // Увеличивает лимит для предупреждений
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Сегментировать библиотеки в отдельный чанк
+          }
+        },
+      },
+    },
   },
 })
